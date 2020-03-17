40c0a933c0f80f533af666987d8ab96306d3fcb8
lenconda <i@lenconda.top>
2020-03-17 18:13:51 +0800

    optimize card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './index.less';
 
-export interface CardProps {
+export interface CardProps extends React.HtmlHTMLAttributes<HTMLElement> {
   color?: string;
   backgroundColor?: string;
   title: string;
   subTitle: string;
-  link: string;
+  href: string;
 }
 
-const Card: React.FC<CardProps> = ({ color = '#333', backgroundColor = 'white', ...props }: CardProps) => {
+const Card: React.FC<CardProps> = ({
+  color = '#333',
+  backgroundColor = 'white',
+  className = '', ...props
+}: CardProps) => {
   return (
-    <a className={styles.card} style={{ backgroundColor, color }} href={props.link}>
+    <div className={`${styles.card} ${className}`} style={{ backgroundColor, color, ...props.style }}>
       <h1>{props.title}</h1>
       <h2>{props.subTitle}</h2>
-    </a>
+      <a href={props.href} className={styles.link}>继续了解</a>
+    </div>
   );
 };
 
