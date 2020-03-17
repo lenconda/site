a22b50753b452f7320ebabd192966ca3b4ea31cb
lenconda <i@lenconda.top>
2020-03-17 18:14:50 +0800

    finish home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -8,19 +8,31 @@ export interface INavigationItem {
 }
 
 const HomePage: React.FC = () => {
-  const [navigations, setNavigations] = useState<INavigationItem[]>([]);
+  const [navigation, setNavigation] = useState<INavigationItem[]>([]);
 
   useEffect(() => {
     http
-      .get('/6cf88eff3c829f6dbe6f779e2170c196e033c9ad/navigations.json')
-      .then(res => res.data && setNavigations(res.data));
+      .get('/navigations.json')
+      .then(res => res.data && setNavigation(res.data));
   }, []);
 
   return (
     <>
-      <div className={styles.navigations}>
+      <div className={styles.navigation}>
         {
-          navigations && navigations.map((value, index) => <a key={index} href={value.href}>{value.name}</a>)
+          navigation && navigation.map((value, index) => {
+            return (
+              <div
+                key={index}
+                className="animated bounceIn"
+                style={{ animationDelay: `${index * 200}ms` }}
+              >
+                <a href={value.href}>
+                  {value.name}
+                </a>
+              </div>
+            );
+          })
         }
       </div>
     </>
