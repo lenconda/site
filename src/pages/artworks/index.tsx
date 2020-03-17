2deb02fb7d9108dfa6ecc1751573af4396402bb2
lenconda <i@lenconda.top>
2020-03-17 18:14:37 +0800

    optimize artwork page

--- src/pages/artworks/index.tsx
+++ src/pages/artworks/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import styles from './index.less';
 
 const ArtworksPage: React.FC = () => {
-  return (<div></div>);
+  return (
+    <div className={`${styles.container} animated slideInUp`}>asdasd</div>
+  );
 };
 
 export default ArtworksPage;
