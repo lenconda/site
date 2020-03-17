326b167491cd8364a73fbda3ab6853f6b7236ba7
lenconda <i@lenconda.top>
2020-03-17 18:14:59 +0800

    finish projects page

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styles from './index.less';
+import Card from '../../components/Card';
+import http from '../../utils/http';
+
+export interface IProjectItem {
+  name: string;
+  description: string;
+  href: string;
+}
 
 const ProjectsPage: React.FC = () => {
-  return (<div></div>);
+  const [projects, setProjects] = useState<IProjectItem[]>(null);
+
+  useEffect(() => {
+    http
+      .get('/projects.json')
+      .then(res => res.data && setProjects(res.data));
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      {
+        projects && projects.map((value, index) => {
+          return (
+            <Card
+              key={index}
+              style={{ animationDelay: `${index * 100 + 360}ms` }}
+              className="animated flipInX"
+              backgroundColor="transparent"
+              color="#333"
+              title={value.name}
+              subTitle={value.description}
+              href={value.href}
+            />
+          );
+        })
+      }
+    </div>
+  );
 };
 
 export default ProjectsPage;
